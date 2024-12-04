864162b2ee6502ba00ae79048e9d581e1bc499b3
bllin001 <bllin001@odu.edu>
2024-12-04 00:31:45 -0500

    Fix Biden

--- code/llama3.py
+++ code/llama3.py
@@ -1,6 +1,6 @@
 import ollama
 
-text = 'President Joe Bide criticized the Supreme Court’s decision on the redistricting of the South Carolina district by issuing a public statement highlighting concerns about racial discrimination.'
+text = 'President Joe Biden criticized the Supreme Court’s decision on the redistricting of the South Carolina district by issuing a public statement highlighting concerns about racial discrimination.'
 
 prompt = f"I want you to characterize the following text into actors, factors, and mechanisms. Actors can range from governmental bodies to grassroots movements, each playing a distinct role in the narrative construction. Factors encompass the various elements influencing the narrative, such as values, interests, and divergences among stakeholders. Conversely, mechanisms delve into the actors’ actions, strategies, and processes to advance their narratives and agendas. {text}"
 
